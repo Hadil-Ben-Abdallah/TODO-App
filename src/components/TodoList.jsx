7b9d124ed51952f6a3ccc26cf755eebe2fefdb63
Hadil <hadilbenabdallah111@gmail.com>
2024-10-04 00:01:03 +0100

    Update issue of last arrow
    The issue was while clicking on the last arrow, everything disappears

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -33,7 +33,7 @@ function TodoList() {
         }
     }
     function moveTaskDown(index) {
-        if (index < tasks.length) {
+        if (index < tasks.length - 1) {
             const updatedTasks = [...tasks];
             [updatedTasks[index], updatedTasks[index + 1]] = [updatedTasks[index + 1], updatedTasks[index]];
             setTasks(updatedTasks);
